81365117ef91a552b56955825cb9f3b8a07036f9
prameela <premi.chekuri@gmail.com>
2022-09-17 15:27:40 +0530

    Added date command substitution

--- 03-vars.sh
+++ 03-vars.sh
@@ -7,4 +7,4 @@ echo value of a = $a
 
 # command substitution means executing command and that particular command output goes to variables
 DATE = $(date)
-echo Today's date is $DATE
+echo Today date is $DATE
