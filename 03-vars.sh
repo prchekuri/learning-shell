7644cb8d9009d42fa65d27e8a74bf69433e68214
prameela <premi.chekuri@gmail.com>
2022-09-17 15:34:40 +0530

    Variables

--- 03-vars.sh
+++ 03-vars.sh
@@ -8,3 +8,7 @@ echo value of a = $a
 # command substitution means executing command and that particular command output goes to variables
 DATE=$(date)
 echo Today date is $DATE
+
+#Arithmetic subs
+ADD=$((2+3+5-1))
+echo Addition = $ADD
