fe534f57624f5bca5990528e5dfc57558b1e4e80
prameela <premi.chekuri@gmail.com>
2022-09-21 00:57:35 +0530

    Demonstration of functions

--- 07-functions.sh
+++ 07-functions.sh
@@ -1,7 +1,11 @@
 #Declaration way1
 
 function sample(){
+  echo value of a from main program $a
+  a=100
+  echo value of a after overwriting in function $a
   echo This is from sample Function
+  b=200
 }
 
 ##Declaration way2
@@ -9,5 +13,9 @@ sample1(){
   echo This is from sample1 Function
 }
 
+a=10
+echo value of b from function $b
+b=20
+echo value of b after overwriting in main program $b
 sample
 sample1
