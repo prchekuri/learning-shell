8f0d5b0da0eab389b9b31bcbe5282eb19818093e
prameela <premi.chekuri@gmail.com>
2022-09-21 00:43:44 +0530

    Demonstration of functions

--- 07-functions.sh
+++ 07-functions.sh
@@ -1,12 +1,12 @@
 #Declaration way1
 
 function sample(){
-  echo This is from sample function
+  echo This is from sample Function
 }
 
 ##Declaration way2
 sample1(){
-  echo This is from sample1 function
+  echo This is from sample1 Function
 }
 
 sample
