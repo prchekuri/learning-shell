0965476a9e62e888799f19b98af1dee3d25e264d
prameela <premi.chekuri@gmail.com>
2022-10-01 16:23:56 +0530

    loops

---
+++ 09-loops.sh
@@ -0,0 +1,19 @@
+# There are two basic loop commands
+# If we want to execute any set of commands again and again in continuous iterative way then we use loops
+
+# while and for loops
+
+# while loop
+a=10
+while [ $a -gt 0 ]; do
+  echo Hello $a
+  a = $(($a-1))
+done
+
+# Loop continuously executes until the condition/expression fails
+
+# for loop
+
+for fruit in apple banana orange ; do
+  echo Fruit Name = $fruit
+done
