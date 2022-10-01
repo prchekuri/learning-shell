6620531094659a0f131c417789ce38f0cb06b80d
prameela <premi.chekuri@gmail.com>
2022-10-01 17:05:46 +0530

    Covered topics list

--- 09-loops.sh
+++ 09-loops.sh
@@ -6,8 +6,12 @@
 # while loop
 a=10
 while [ $a -gt 0 ]; do
+  if [ $a -eq 5 ]; then
+    break
+  fi
   echo Hello $a
   a=$(($a-1))
+  sleep 1
 done
 
 # Loop continuously executes until the condition/expression fails
