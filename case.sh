988d872585f680b13af9035bcc43ba37d20a871c
prameela <premi.chekuri@gmail.com>
2022-10-01 16:02:02 +0530

    case command

---
+++ case.sh
@@ -0,0 +1,14 @@
+system=Linux
+
+case $system in
+  Linux) echo "Linux System"
+    ;;
+  Unix) echo "Unix"
+    ;;
+esac
+
+#syntax
+#case $var in
+# patt1) commands ;;
+# patt2) commnads ;;
+#esac
