02134e25bdcc2ef43671d39c982598ee843eca19
prameela <premi.chekuri@gmail.com>
2022-10-01 16:11:13 +0530

    Print

---
+++ 02-print.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo Hello World
+
+#print output message in colors
+#color print syntax
+#echo -e "\e[COLmMESSAGE\e[0m"
+#echo -e "\e[31mHELLO\e[0m" ; echo World
+#\e[31m -- to enable color code 31
+#\e[0m -- to disable the enabled color we use 0
+#-e -- to enable escape sequence, \e is one esc seq
+#"" -- quotes are mandatory if we use esc seq
+
+##colors
+#red      31
+#green    32
+#yellow   33
+#blue     34
+#magenta  35
+#cyan     36
